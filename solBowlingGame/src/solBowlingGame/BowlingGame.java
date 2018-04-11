8bdc7ac76661bdbd4e19cd1a84ed70e969e27d1d
sabbir21 <syedsabbirahmed21.2@gmail.com>
2018-04-11 02:02:12 -0400

    Bowling Game Solution 3

--- solBowlingGame/src/solBowlingGame/BowlingGame.java
+++ solBowlingGame/src/solBowlingGame/BowlingGame.java
@@ -15,41 +15,28 @@ public class BowlingGame {
 
 	public int score() {
 		int score = 0;
-		int cursor = 0;
+		int pointer = 0;
 
 		for (int i = 0; i < 10; i++) {
-			if (isStrike(cursor)) {
-				score += 10 + strikeBonus(cursor);
-				cursor++;
-			} else if (isSpare(cursor)) {
-				score += 10 + spareBonus(cursor);
-				cursor += 2;
-			} else {
-				score += sumOfRolls(cursor);
-				cursor += 2;
+			
+			//count strike point
+			if (rolls[pointer] == 10) {
+				score += 10 + rolls[pointer+1] + rolls[pointer+2];
+				pointer++;
+			} 
+			//count spare point
+			else if (rolls[pointer] + rolls[pointer+1] == 10) {
+				score += 10 + rolls[pointer+2];
+				pointer += 2;
+			} 
+			//normal scoring
+			else {
+				score += (rolls[pointer] + rolls[pointer+1]);
+				pointer += 2;
 			}
 		}
 
 		return score;
 	}
-
-	private boolean isStrike(int cursor) {
-		return rolls[cursor] == 10;
-	}
-
-	private boolean isSpare(int cursor) {
-		return sumOfRolls(cursor) == 10;
-	}
-
-	private int strikeBonus(int cursor) {
-		return sumOfRolls(cursor+1);
-	}
-
-	private int spareBonus(int cursor) {
-		return rolls[cursor+2];
-	}
-
-	private int sumOfRolls(int cursor) {
-		return rolls[cursor] + rolls[cursor+1];
-	}
+	
 }
